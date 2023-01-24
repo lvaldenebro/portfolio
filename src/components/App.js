08fcb1a3e816c92d33148db3ce9e38805b1cdf78
lvaldenebro <lauravaldeal@gmail.com>
2023-01-24 16:51:37 +0100

    Adds boostrap style to navs

--- src/components/App.js
+++ src/components/App.js
@@ -29,150 +29,165 @@ import zeplin from '../images/zeplin.svg';
 
 
 function App() {
-  return (
+return (
     <div>
-      <header className="container-fluid border">
-          {/* <!-- TODO: ojo a la navigation para mobile con el icono --> */}
-          <nav>
+    <nav>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">About Me</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tech Stack</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+ {/* <header className="container-fluid border my-5">
+          <nav className="row border align-items-end gx-2">
               <ul>
-                  <li>About Me</li>
-                  <li>Tech Stack</li>
-                  <li>Projects</li>
-                  <li>Contact</li>
+                  <li className="col-sm-8 border justify-content-center list">About Me</li>
+                  <li className="col-sm-8 border justify-content-center list">Tech Stack</li>
+                  <li className="col-sm-8 border justify-content-center list">Projects</li>
+                  <li className="col-sm-8 border justify-content-center list">Contact</li>
               </ul>
           </nav>
-      </header>
+      </header> */}
 
-      <main>
-          <section className="container-md">
-              <img src= {avatar} title="Laura" alt="Laura Valdenebro" />
-              <h1>Laura Valdenebro</h1>
-              <p>Full-stack developer</p>
-              <p>Highlight you passion 💙</p>
-          </section>
+    <main>
+        <section className="container-fluid border">
+            <img src= {avatar} title="Laura" alt="Laura Valdenebro" />
+            <h1>Laura Valdenebro</h1>
+            <p>Full-stack developer</p>
+            <p>Highlight you passion 💙</p>
+        </section>
 
-          <section className="container-fluid">
-              <h2>About Me</h2>
-                  <p>Horse jumping requires planning, discipline and practice, but also such a strong link between rider and animal that they act as a single being. That's me, a team player with a unique and different perspective that helps me find innovative solutions to all problems. Always in a crusade for self-explicative and super-effective code, I'm looking for a place to apply all my experience in tech while I evolve and learn as a fullstack developer. My motto: "try, learn, share".</p>
-                  {/* <!-- TODO: ojo con el icono custom para el cv y su link --> */}
-                  <p>Resumé</p>
-          </section>
+        <section className="container-fluid">
+            <h2>About Me</h2>
+                <p>Horse jumping requires planning, discipline and practice, but also such a strong link between rider and animal that they act as a single being. That's me, a team player with a unique and different perspective that helps me find innovative solutions to all problems. Always in a crusade for self-explicative and super-effective code, I'm looking for a place to apply all my experience in tech while I evolve and learn as a fullstack developer. My motto: "try, learn, share".</p>
+                {/* <!-- TODO: ojo con el icono custom para el cv y su link --> */}
+                <p>Resumé</p>
+        </section>
 
-          <section className="container-fluid">
-              <h2>Tech Stack</h2>
-                  {/* <!-- TODO: revisar la lista --> */}
-                  <ul>
-                      <li><img src= { html } title="html" alt="html logo" /></li>
-                      <li><img src= { css } title="css" alt="css logo" /></li>
-                      <li><img src= { sass } title="sass" alt="sass logo" /></li>
-                      <li><img src= { js } title="js" alt="js logo" /></li>
-                      <li><img src= { python } title="python" alt="python logo" /></li>
-                      <li><img src= { bash } title="bash" alt="bash logo" /></li>
-                      <li><img src= { git } title="git" alt="git logo" /></li>
-                      <li><img src= { github } title="github" alt="github logo" /></li>
-                      <li><img src= { bootstrap } title="bootstrap" alt="bootstrap logo" /></li>
-                      <li><img src= { nodejs } title="nodejs" alt="nodejs logo" /></li>
-                      <li><img src= { sql } title="sql" alt="sql logo" /></li>
-                      <li><img src= { sqlite } title="sqlite" alt="sqlite logo" /></li>
-                      <li><img src= { zeplin } title="zeplin" alt="zeplin logo" /></li>
-                      <li><img src= { canva } title="canva" alt="canva logo" /></li>
-                      <li><img src= { react } title="react" alt="react logo" /></li>
-                      <li>APIs Rest</li>
-                      <li>Miro</li>
-                      <li>Agile</li>
-                      <li>Scrum</li>
-                  </ul>
-          </section>
+        <section className="container-fluid">
+            <h2>Tech Stack</h2>
+                {/* <!-- TODO: revisar la lista --> */}
+                <ul>
+                    <li><img src= { html } title="html" alt="html logo" /></li>
+                    <li><img src= { css } title="css" alt="css logo" /></li>
+                    <li><img src= { sass } title="sass" alt="sass logo" /></li>
+                    <li><img src= { js } title="js" alt="js logo" /></li>
+                    <li><img src= { python } title="python" alt="python logo" /></li>
+                    <li><img src= { bash } title="bash" alt="bash logo" /></li>
+                    <li><img src= { git } title="git" alt="git logo" /></li>
+                    <li><img src= { github } title="github" alt="github logo" /></li>
+                    <li><img src= { bootstrap } title="bootstrap" alt="bootstrap logo" /></li>
+                    <li><img src= { nodejs } title="nodejs" alt="nodejs logo" /></li>
+                    <li><img src= { sql } title="sql" alt="sql logo" /></li>
+                    <li><img src= { sqlite } title="sqlite" alt="sqlite logo" /></li>
+                    <li><img src= { zeplin } title="zeplin" alt="zeplin logo" /></li>
+                    <li><img src= { canva } title="canva" alt="canva logo" /></li>
+                    <li><img src= { react } title="react" alt="react logo" /></li>
+                    <li>APIs Rest</li>
+                    <li>Miro</li>
+                    <li>Agile</li>
+                    <li>Scrum</li>
+                </ul>
+        </section>
 
-          <section className="container-fluid">
-              <h2>Projects</h2>
-              {/* <!-- TODO: carrousel? POR todos los proyectos. Partial vacío--> */}
-              <ul>
-                  <li>
-                      <img src="http://placekitten.com/200/300" alt="Image of the project" />
-                      <h3>Criature's battle</h3>
-                          <p>Game player vs. machine between the good and the bad forces in the Middle Earth</p>
-                          <p>JavaScript | HTML | Sass</p>
-                          <ul>
-                              <li>
-                                <a href="https://lvaldenebro.github.io/creatures-battle/">
-                                    <img src={ webPage } alt="Demo" />
-                                </a>
-                              </li>
-                              <li>
-                                <a href="https://github.com/lvaldenebro/creatures-battle">
-                                    <img src={ code } alt="Web page0's code" />
-                                </a>
-                              </li>
-                          </ul>
-                    </li>
-                    <li>
-                      <img src="http://placekitten.com/200/300" alt="Image of the project" />
-                      <h3>Criature's battle</h3>
-                          <p>Game player vs. machine between the good and the bad forces in the Middle Earth</p>
-                          <p>JavaScript | HTML | Sass</p>
-                          <ul>
-                              <li>
-                                <a href="https://lvaldenebro.github.io/creatures-battle/">
-                                    <img src={ webPage } alt="Demo" />
-                                </a>
-                              </li>
-                              <li>
-                                <a href="https://github.com/lvaldenebro/creatures-battle">
-                                    <img src={ code } alt="Web page0's code" />
-                                </a>
-                              </li>
-                          </ul>
-                    </li>
-              </ul>
-          </section>
+        <section className="container-fluid">
+            <h2>Projects</h2>
+            {/* <!-- TODO: carrousel? POR todos los proyectos. Partial vacío--> */}
+            <ul>
+                <li>
+                    <img src="http://placekitten.com/200/300" alt="Image of the project" />
+                    <h3>Criature's battle</h3>
+                        <p>Game player vs. machine between the good and the bad forces in the Middle Earth</p>
+                        <p>JavaScript | HTML | Sass</p>
+                        <ul>
+                            <li>
+                            <a href="https://lvaldenebro.github.io/creatures-battle/">
+                                <img src={ webPage } alt="Demo" />
+                            </a>
+                            </li>
+                            <li>
+                            <a href="https://github.com/lvaldenebro/creatures-battle">
+                                <img src={ code } alt="Web page0's code" />
+                            </a>
+                            </li>
+                        </ul>
+                </li>
+                <li>
+                    <img src="http://placekitten.com/200/300" alt="Image of the project" />
+                    <h3>Criature's battle</h3>
+                        <p>Game player vs. machine between the good and the bad forces in the Middle Earth</p>
+                        <p>JavaScript | HTML | Sass</p>
+                        <ul>
+                            <li>
+                            <a href="https://lvaldenebro.github.io/creatures-battle/">
+                                <img src={ webPage } alt="Demo" />
+                            </a>
+                            </li>
+                            <li>
+                            <a href="https://github.com/lvaldenebro/creatures-battle">
+                                <img src={ code } alt="Web page0's code" />
+                            </a>
+                            </li>
+                        </ul>
+                </li>
+            </ul>
+        </section>
 
-          <section className="container-fluid">
-              <h2>Contact</h2>
-              {/* <!-- TODO: carrousel? POR todos los proyectos. Partial vacío--> */}
-              <ul>
-                  <li>
-                      {/* <!-- TODO: dowload cv --> */}
-                      <img src={ cv } title="CV" alt="Cv's icon" />
-                      <p>Know me!</p>
-                  </li>
-                  <li>
-                      {/* <!-- TODO: link --> */}
-                      <img src={ github } title= "GitHub" alt="Github's icon" />
-                      <p>Fork me!</p>
-                  </li>
-                  <li>
-                      {/* <!-- TODO: milto --> */}
-                      <img src= { email } title="email" alt="Email's icon" />
-                      <p>Contact me!</p>
-                  </li>
-                  <li>
-                      {/* <!-- TODO: link --> */}
-                      <img src={ linkedin } title="linkedin" alt="Linkedin's icon" />
-                      <p>Hire me!</p>
-                  </li>
-              </ul>
-          </section>
-      </main>
+        <section className="container-fluid">
+            <h2>Contact</h2>
+            {/* <!-- TODO: carrousel? POR todos los proyectos. Partial vacío--> */}
+            <ul>
+                <li>
+                    {/* <!-- TODO: dowload cv --> */}
+                    <img src={ cv } title="CV" alt="Cv's icon" />
+                    <p>Know me!</p>
+                </li>
+                <li>
+                    {/* <!-- TODO: link --> */}
+                    <img src={ github } title= "GitHub" alt="Github's icon" />
+                    <p>Fork me!</p>
+                </li>
+                <li>
+                    {/* <!-- TODO: milto --> */}
+                    <img src= { email } title="email" alt="Email's icon" />
+                    <p>Contact me!</p>
+                </li>
+                <li>
+                    {/* <!-- TODO: link --> */}
+                    <img src={ linkedin } title="linkedin" alt="Linkedin's icon" />
+                    <p>Hire me!</p>
+                </li>
+            </ul>
+        </section>
+    </main>
 
-      <footer className="container-fluid">
-          <nav>
-              {/* <!-- <nav> TODO: misma que arriba --> */}
-              <ul>
-                  <li>About Me</li>
-                  <li>Tech Stack</li>
-                  <li>Projects</li>
-                  <li>Contact</li>
-              </ul>
-          </nav>
-          <div>
-              <p>Designed and built by Laura Valdenebro | 2023</p>
-              <p>Highlight your passion! 💙</p>
-          </div>
-      </footer>
+    <footer className="container-fluid">
+            {/* <!-- <nav> TODO: revisar los enlaces --> */}
+        <nav class="nav flex-column">
+            <a class="nav-link active" aria-current="page" href="#">About Me</a>
+            <a class="nav-link" href="#">Tech Stack</a>
+            <a class="nav-link" href="#">Projects</a>
+            <a class="nav-link"  href="#">Contact</a>
+        </nav>
 
-    </div>
-  );
+        <div>
+            <p>Designed and built by Laura Valdenebro | 2023</p>
+            <p>Highlight your passion! 💙</p>
+        </div>
+    </footer>
+
+</div>
+);
 }
 
 export default App;
