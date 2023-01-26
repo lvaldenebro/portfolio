49255b05f3b3f08dcd8e8dce6c64be9c6b32c675
lvaldenebro <lauravaldeal@gmail.com>
2023-01-26 13:40:52 +0100

    Changes the CV for one without personal information and adds all the relevant information for each project

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,17 @@ import '../styles/App.scss';
 
 //IMAGES
 import avatar from '../images/avatar.jpg';
-import criatures from '../images/criatures-battle.png';
+import criaturesBattle from '../images/projects/criatures-battle.png';
+import landingPage from '../images/projects/landing-page.png';
+import simpleLandingPage from '../images/projects/simple-landing-page.png';
+import adalaberManagement from '../images/projects/adalaber-management.png';
+import minionGenerator from '../images/projects/minion-generator.png';
+import bbSearcher from '../images/projects/BB-character-searcher.png';
+import introductoryLanding from '../images/projects/introductory-landing.png';
+import reactRefactor from '../images/projects/react-refactor.png';
+import rmSearcher from '../images/projects/RM-character-searcher.png';
+import bookHostel from '../images/projects/book-hostel.png';
+import portfolio from '../images/projects/portfolio.png';
 
 
 //DOCUMENTS
@@ -55,16 +65,16 @@ return (
     </nav>
 
     <main>
-        <section className="container border">
+        <section className="container">
             <div className="row introduction-div">
                 {/* mx-auto + d-block center the image */}
                 {/* TODO: review the picture is not being centered */}
-                <div className="col-md-6 border circle-box">
+                <div className="col-md-6 circle-box">
                     <div className="circle">
                         <img className="img" src= {avatar} title="Laura" alt="Laura Valdenebro" />
                     </div>
                 </div>
-                <div className="col-md-6 text-center text-md-start border">
+                <div className="col-md-6 text-center text-md-start">
                     <h1 className="h1">Laura Valdenebro</h1>
                     <p className="subtitle">Full-stack developer</p>
                     <p className="motto">Highlight your passion💙</p>
@@ -115,42 +125,78 @@ return (
             <div className="row row-projects">
                 <h2 className="h2"><span className="highlight-container"><span className="highlight">Projects</span></span></h2>
             </div>
-
             <div className="row row-projects important-projects">
-                <div className="card text-center">
-                    <img src={ criatures } className="card-img-top" alt="..." />
+
+            <div className="card text-center">
+                    <img src={bookHostel} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Criature's battle</h5>
-                        <p className="card-text">Game player vs. machine between the good and the bad forces in the Middle Earth</p>
+                        <h5 className="card-title">BookHostel (under construction)</h5>
+                        <p className="card-text">A page in which exchange freely your old books</p>
                         <hr />
-                        <p className="card-text">JavaScript | HTML | Sass</p>
+                        <p className="card-text">HTML5 | Sass | React | Bootstrap | API Rest | NextJS | ExpressJS | DB</p>
                         <hr />
-                        <a className="btn btn-primary" href="https://lvaldenebro.github.io/creatures-battle/">
+                        {/* <a className="btn btn-primary" href="#">
+                            <img  className="logo" src={ webPage } alt="Demo" />
+                        </a> */}
+                        <a className="btn btn-primary" href="https://github.com/lvaldenebro/book-hostel">
+                            <img  className="logo" src={ code } alt="Web page's code" />
+                        </a>
+                    </div>
+                </div>
+
+            <div className="card text-center">
+                    <img src={portfolio} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Porfolio (Welcome to the v 1.0!)</h5>
+                        <p className="card-text">Portfolio actively being improved to show what I can do</p>
+                        <hr />
+                        <p className="card-text">HTML5 | Sass | React | Bootstrap</p>
+                        <hr />
+                        <a className="btn btn-primary" href="www.lauravaldenebro.com">
                             <img  className="logo" src={ webPage } alt="Demo" />
                         </a>
-                        <a className="btn btn-primary" href="https://github.com/lvaldenebro/creatures-battle">
-                            <img  className="logo" src={ code } alt="Web page0's code" />
+                        <a className="btn btn-primary" href="https://github.com/lvaldenebro/portfolio">
+                            <img  className="logo" src={ code } alt="Web page's code" />
                         </a>
                     </div>
                 </div>
 
                 <div className="card text-center">
-                    <img src={ criatures } className="card-img-top" alt="..." />
+                    <img src={ bbSearcher } className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">Criature's battle</h5>
-                        <p className="card-text">Game player vs. machine between the good and the bad forces in the Middle Earth</p>
+                        <h5 className="card-title">Breaking bad, dead or alive?</h5>
+                        <p className="card-text">Breaking Bad based character searcher</p>
                         <hr />
-                        <p className="card-text">JavaScript | HTML | Sass</p>
+                        <p className="card-text">HTML5 | Sass | JS | localeStorage | API Rest</p>
                         <hr />
-                        <a className="btn btn-primary" href="https://lvaldenebro.github.io/creatures-battle/">
+                        <a className="btn btn-primary" href="https://lvaldenebro.github.io/breaking-bad-character-searcher-js/">
                             <img  className="logo" src={ webPage } alt="Demo" />
                         </a>
-                        <a className="btn btn-primary" href="https://github.com/lvaldenebro/creatures-battle">
-                            <img  className="logo" src={ code } alt="Web page0's code" />
+                        <a className="btn btn-primary" href="https://github.com/lvaldenebro/breaking-bad-character-searcher-js">
+                            <img  className="logo" src={ code } alt="Web page's code" />
+                        </a>
+                    </div>
+                </div>
+
+                <div className="card text-center">
+                    <img src={ rmSearcher } className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Rick and Morty, who is what?</h5>
+                        <p className="card-text">Rick and Morty's based character searcher</p>
+                        <hr />
+                        <p className="card-text">HTML5 | Sass | React | React Router | localStorage | API Rest</p>
+                        <hr />
+                        <a className="btn btn-primary" href="https://lvaldenebro.github.io/rick-morty-character-searcher-react/">
+                            <img  className="logo" src={ webPage } alt="Demo" />
+                        </a>
+                        <a className="btn btn-primary" href="https://github.com/lvaldenebro/rick-morty-character-searcher-react">
+                            <img  className="logo" src={ code } alt="Web page's code" />
                         </a>
                     </div>
                 </div>
             </div>
+
+
 
 {/* 
 carousel -- for less important projects*/}
@@ -160,44 +206,106 @@ carousel -- for less important projects*/}
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="6" aria-label="Slide 7"></button>
+
                 </div>
+
                 <div className="carousel-inner">
+
                     <div className="carousel-item active" data-bs-interval="10000">
-                        <img src={ criatures } className="d-block w-100" alt="..."/>
+                        <img src={ landingPage } className="d-block w-100" alt="..."/>
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Criature's battle</h5>
-                            <a className="btn btn-primary" href="https://lvaldenebro.github.io/creatures-battle/">
+                            <a className="btn btn-primary" href="https://lvaldenebro.github.io/landing-page/">
                                 <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
                             </a>
-                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/creatures-battle">
-                                <img  className="logo logo-carousel" src={ code } alt="Web page0's code" />
-                            </a>
-                    </div>
-                    </div>
-                    <div className="carousel-item" data-bs-interval="2000">
-                        <img src={ criatures } className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Criature's battle</h5>
-                            <a className="btn btn-primary" href="https://lvaldenebro.github.io/creatures-battle/">
-                                <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
-                            </a>
-                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/creatures-battle">
-                                <img  className="logo logo-carousel" src={ code } alt="Web page0's code" />
+                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/landing-page">
+                                <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
                             </a>
                         </div>
                     </div>
+
                     <div className="carousel-item">
-                        <img src={ criatures } className="d-block w-100" alt="..." />
+                        <img src={ criaturesBattle } className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
                             <h5>Criature's battle</h5>
                             <a className="btn btn-primary" href="https://lvaldenebro.github.io/creatures-battle/">
                                 <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
                             </a>
                             <a className="btn btn-primary" href="https://github.com/lvaldenebro/creatures-battle">
-                                <img  className="logo logo-carousel" src={ code } alt="Web page0's code" />
+                                <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
                             </a>
                         </div>
                     </div>
+
+                    <div className="carousel-item">
+                        <img src={ reactRefactor } className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>React Refactor Challenge</h5>
+                            <a className="btn btn-primary" href="https://react-refactor-challenge-production.up.railway.app/">
+                                <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
+                            </a>
+                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/react-refactor-challenge">
+                                <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item">
+                        <img src={ minionGenerator } className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Minion Card Generator</h5>
+                            <a className="btn btn-primary" href="https://lvaldenebro.github.io/minion-card-generator/">
+                                <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
+                            </a>
+                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/minion-card-generator">
+                                <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item">
+                        <img src={ introductoryLanding } className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Team Introduction Landing</h5>
+                            <a className="btn btn-primary" href="https://lvaldenebro.github.io/team-introduction-landing/">
+                                <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
+                            </a>
+                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/team-introduction-landing">
+                                <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item">
+                        <img src={ adalaberManagement } className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Adalaber's Info Management</h5>
+                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/adalaber-management">
+                                <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
+                            </a>
+                            <a className="btn btn-primary" href="https://lvaldenebro.github.io/adalaber-management/">
+                                <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item" data-bs-interval="2000">
+                        <img src={ simpleLandingPage } className="d-block w-100" alt="..." />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Simple Landing Page</h5>
+                            <a className="btn btn-primary" href="https://lvaldenebro.github.io/simple-landing-page/">
+                                <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
+                            </a>
+                            <a className="btn btn-primary" href="https://github.com/lvaldenebro/simple-landing-page">
+                                <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
+                            </a>
+                        </div>
+                    </div>
+
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
