e75954e98a2cfe90a811c30e069a6a829d21f9b4
lvaldenebro <lauravaldeal@gmail.com>
2023-02-07 12:29:39 +0100

    Deletes arrow component

--- src/components/footer/Arrow.js
+++
@@ -1,9 +0,0 @@
-const Arrow = () => {
-    
-    return (
-        <div className="row arrow">
-            <a className="justify-content-end" href="#header"><p>⬆️</p></a>
-        </div>
-)};
-
-export default Arrow;
