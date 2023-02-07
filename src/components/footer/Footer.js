3a4ba5a0daf015f0ec2f22336258773b4f2e26bf
lvaldenebro <lauravaldeal@gmail.com>
2023-02-07 12:32:12 +0100

    Deletes arrow component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,9 @@
 import NavList from '../general/NavList';
-import Arrow from './Arrow';
 import Credits from './Credits';
 
 const Footer = () => {
     return (
         <footer className="container-fluid footer section text-center">
-            <Arrow />
             <div className="row">
                 <div className="col">
                     <NavList />
