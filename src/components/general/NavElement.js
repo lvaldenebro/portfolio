d0afa514cc16a4c30d1002690228bfb0f8b75450
lvaldenebro <lauravaldeal@gmail.com>
2023-02-07 20:56:45 +0100

    Fixes anchor links

--- src/components/general/NavElement.js
+++ src/components/general/NavElement.js
@@ -2,7 +2,7 @@ const NavElement = (props) => {
     return (
         <>
             <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#{props.href}"><span className="highlight-container"><span className="highlight">{props.text}</span></span></a>
+                <a className="nav-link active" aria-current="page" href={`#${props.href}`}><span className="highlight-container"><span className="highlight">{props.text}</span></span></a>
             </li>
         </>
 )};
