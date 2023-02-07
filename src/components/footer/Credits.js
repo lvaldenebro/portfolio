eb652ce3e173c431254ad230d0f72e3beeb7483b
lvaldenebro <lauravaldeal@gmail.com>
2023-02-07 15:25:40 +0100

    Adds hight to motto into footer

--- src/components/footer/Credits.js
+++ src/components/footer/Credits.js
@@ -3,7 +3,7 @@ const Credits = () => {
     return (
         <div className="col credits">
             <p>Designed and built by Laura Valdenebro | 2023</p>
-            <p>Highlight your passion!<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-heart heart-footer" viewBox="0 0 24 24">
+            <p><span className="highlight-container"><span className="highlight">Highlight</span></span> your passion!<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-heart heart-footer" viewBox="0 0 24 24">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
             </svg></p>
