8b8760db25dff043e7c5f2f71e9f5a3a3779ecd6
lvaldenebro <lauravaldeal@gmail.com>
2023-02-10 12:21:04 +0100

    Fixes the indentantion of ProjectCarouselItem

--- src/components/projects/ProjectCarouselItem.js
+++ src/components/projects/ProjectCarouselItem.js
@@ -9,16 +9,15 @@ const ProjectCarouselItem = ( props ) => {
             {/* <div className="carousel-caption d-none d-md-block"> */}
             <div className="carousel-caption d-none d-md-inline-flex carousel-info">
                 <div className="carousel-projects-title-box">
-                <h5 className="carousel-projects-title">{ props.title }</h5>
+                    <h5 className="carousel-projects-title">{ props.title }</h5>
                 </div>
                 <div className="carousel-projects-logos-box">
-                <a className="btn btn-projects" href={ props.web}>
-                    <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
-                </a>
-
-                <a className="btn btn-projects" href={props.github}>
-                    <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
-                </a>
+                    <a className="btn btn-projects" href={ props.web}>
+                        <img  className="logo logo-carousel" src={ webPage } alt="Demo" />
+                    </a>
+                    <a className="btn btn-projects" href={props.github}>
+                        <img  className="logo logo-carousel" src={ code } alt="Web page's code" />
+                    </a>
                 </div>
             </div>
         </div>
